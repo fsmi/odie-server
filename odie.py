5ad70fc73937f5ddee9fb2ca2df5f1ca1ddbbbb1
Max Wagner <max@trollbu.de>
2015-04-24 01:56:10 +0200

    Implement login logic; customize api a bit

--- odie.py
+++ odie.py
@@ -1,17 +1,30 @@
 #! /usr/bin/env python3
 
 import app
+import routes
+
+from flask.ext.restless import APIManager, ProcessingException
+from flask.ext.login import current_user
+
 from app import app, db
 from models import documents
 from models import odie
 
-from flask.ext.restless import APIManager
+
+def auth_preproc(**kw):
+    if not current_user.is_authenticated():
+        raise ProcessingException(description='Not Authorized', code=401)
 
 manager = APIManager(app, flask_sqlalchemy_db=db)
-manager.create_api(documents.Lecture)
-manager.create_api(documents.Document)
+manager.create_api(documents.Lecture,
+        exclude_columns=['documents'])
+manager.create_api(documents.Document,
+        exclude_columns=['legacy_id', 'file_id'])
 manager.create_api(documents.Examinant)
-manager.create_api(documents.Deposit)
+manager.create_api(
+        documents.Deposit,
+        methods=['GET', 'POST'],
+        preprocessors={'GET_SINGLE': [auth_preproc], 'GET_MANY': [auth_preproc], 'POST': [auth_preproc]})
 manager.create_api(odie.Cart, methods=['POST', 'GET'])
 
 app.run()
