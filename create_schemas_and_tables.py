8cb20ca2713cb82e0bfc679bd30f218c30519721
Max Wagner <max@trollbu.de>
2015-05-19 01:31:58 +0200

    Ensure custom db types are created in right db/schema

--- create_schemas_and_tables.py
+++ create_schemas_and_tables.py
@@ -4,7 +4,12 @@
 import sqlalchemy
 import app
 
+import models.documents
+import models.public
+import models.odie
+
 from sqlalchemy.schema import CreateSchema
+from sqlalchemy.dialects import postgres
 from app import db
 
 def createSchema(name, bind=None):
@@ -22,6 +27,17 @@ createSchema('public')
 createSchema('acl', 'fsmi')
 createSchema('odie')
 createSchema('documents')
+
+
+# due to issues with postgres.ENUM's inherit_schema parameter not finding the
+# right db, we need to create all custom types here ahead of time and manually
+# specify the right bind.
+
+garfield = db.get_engine(app.app)
+postgres.ENUM('mathematics', 'computer science', 'both', name='subject', schema='documents').create(bind=garfield)
+postgres.ENUM('official', 'inofficial', 'none', name='solution', schema='documents').create(bind=garfield)
+postgres.ENUM('oral', 'written', 'oral reexam', name='type', schema='documents').create(bind=garfield)
+
 
 # create tables if necessary
 db.create_all()
