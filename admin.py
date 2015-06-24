f26e81bc89d8d97cdd2d1a45e0e1213d5ce6dcd6
Max Wagner <max@trollbu.de>
2015-06-24 20:54:09 +0200

    Configure Document admin view, add 'view pdf' button

--- admin.py
+++ admin.py
@@ -10,6 +10,12 @@ from flask.ext.login import current_user
 
 import datetime
 
+def _dateFormatter(attr_name):
+    def f(v, c, m, n):
+        d = getattr(m, attr_name)
+        return d.date() if d else ''
+    return f
+
 class AuthView(BaseView):
     def is_accessible(self):
         return current_user.is_authenticated()
@@ -21,8 +27,45 @@ class AuthView(BaseView):
 class AuthModelView(ModelView, AuthView):
     pass
 
+
 class DocumentView(AuthModelView):
+    list_template = 'document_list.html'
     form_excluded_columns = ('validation_time', 'file_id')
+    column_list = (
+            'lectures', 'examinants', 'date', 'number_of_pages', 'solution', 'comment',
+            'document_type', 'validated', 'validation_time', 'submitted_by')
+    column_labels = {
+            'lectures': 'Vorlesungen',
+            'examinants': 'Prüfer',
+            'date': 'Datum',
+            'number_of_pages': 'Seiten',
+            'solution': 'Lösung',
+            'comment': 'Kommentar',
+            'document_type': 'Typ',
+            'validated': 'Überprüft',
+            'validation_time': 'Überprüft am',
+            'submitted_by': 'Von',
+        }
+    doctype_labels = {
+            'oral': 'Mündl.',
+            'written': 'Schriftl.',
+            'oral reexam': 'Nachprüfung',
+        }
+    solution_labels = {
+            'official': 'Ja (offiziell)',
+            'inofficial': 'Ja (Studi)',
+            'none': 'Nein',
+            None: '?',
+        }
+    column_formatters = {
+            'document_type': lambda v,c,m,n: DocumentView.doctype_labels[m.document_type],
+            'solution': lambda v,c,m,n: DocumentView.solution_labels[m.solution],
+            'date': _dateFormatter('date'),
+            'validation_time': _dateFormatter('validation_time'),
+        }
+    def scaffold_list_form(self, validators=[], **kwargs):
+        validators = {'name': {'validators': []}}
+        return super(DocumentView, self).scaffold_list_form(validators=validators, **kwargs)
 
     def on_model_change(self, form, model, is_created):
         if model.validation_time is None and model.validated:
@@ -39,4 +82,3 @@ admin.add_view(DocumentView(Document, db.session, name='Dokumente'))
 admin.add_view(LectureView(Lecture, db.session, name='Vorlesungen'))
 admin.add_view(ExaminantView(Examinant, db.session, name='Prüfer'))
 admin.add_view(AuthModelView(Deposit, db.session, name='Pfand'))
-
