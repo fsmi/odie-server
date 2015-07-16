3bf590b6e820182b394a6f98688db53d292142d3
Max Wagner <max@trollbu.de>
2015-07-16 18:00:00 +0200

    Adjust accounting hooks to new version of garfield patch

--- db/accounting.py
+++ db/accounting.py
@@ -16,10 +16,14 @@ the fields of a deserialized PrintJobSchema). Monetary parameters
 the database internally uses in the logging functions
 """
 
-from sqlalchemy import text
+from sqlalchemy import cast, text, String
 from odie import sqla
-import config
 
+import config
+import sqlalchemy
+
+class AccountingException(Exception):
+    pass
 
 # pylint: disable=function-redefined
 def log_donation(user, amount: int, cashbox: str):
@@ -35,6 +39,7 @@ def log_deposit_return(deposit, user, cashbox: str):
 
 if config.GARFIELD_ACCOUNTING:
 
+    procs = sqlalchemy.sql.func.garfield
     _qry = text("""SELECT cash_box_name, cash_boxes.cash_box_id
             FROM garfield.cash_boxes;""")
     cash_box_ids = dict(sqla.session.execute(_qry).fetchall())
@@ -42,60 +47,46 @@ if config.GARFIELD_ACCOUNTING:
     _tax_group = 2
 
 
-    def _cash_box_log_entry(user, cashbox: str, amount: float, action: str):
-        qry = text("""INSERT INTO garfield.cash_box_log
-                (cash_box_log_performed_by_user_id, cash_box_id, cash_box_log_quantity, type_id, receipt_number)
-                SELECT user_id, :cash_box_id, :amount, :type, garfield.cash_box_log_get_receipt_number(:cash_box_id, current_date)
-                FROM garfield.users
-                WHERE user_name = :user_name
-                RETURNING cash_box_log_id;""")
-        r = sqla.session.execute(qry.bindparams(
-            user_name=user.username,
-            cash_box_id=cash_box_ids[cashbox],
-            amount=amount,
-            type=action,
-        ))
-        return r.scalar()
+    def _garfield_user_id(user):
+        qry = text("""SELECT user_id FROM garfield.users WHERE user_name = :username""")
+        r = sqla.session.execute(qry.bindparams(username=user.username)).scalar()
+        if r is None:
+            raise AccountingException('User not found')
+        return r
+
 
     def log_donation(user, amount: int, cashbox: str):
-        # I'd love to use donation_accept, but that thing wants to be smart and
-        # guesses the user id from the session user, which doesn't exist.
-        # So... more raw SQL it is.
-        action = 'DONATION' if amount >= 0 else 'DONATION_CANCEL'
-        log_id = _cash_box_log_entry(user, cashbox, amount / 100, action)
-        qry = text("""INSERT INTO garfield.donation_sales_log
-                VALUES (:log_id, 'MONEY');""")
-        sqla.session.execute(qry.bindparams(log_id=log_id))
+        user_id = _garfield_user_id(user)
+        amount = amount / 100  # garfield uses floats, because of course it does
+        proc = procs.donation_accept(cash_box_ids[cashbox], 'MONEY', amount, user_id)
+        sqla.session.execute(proc).scalar()
+
 
     def _log_exam_action(pages: int, final_price: float, user, cashbox: str, action: str):
-        cshbx_log_id = _cash_box_log_entry(user, cashbox, final_price, action)
-        qry = text("""INSERT INTO garfield.exam_sale_log
-                (cash_box_log_id, tax_id, price_per_page, pages)
-                VALUES (:cshbx_log_id, garfield.tax_find(:tax_group, current_date), :ppp, :pages);""")
-        sqla.session.execute(qry.bindparams(
-            cshbx_log_id=cshbx_log_id,
-            tax_group=_tax_group,
-            ppp=config.FS_CONFIG['PRICE_PER_PAGE'] / 100,
-            pages=pages
-        ))
+        # Without these casts, the strings will end up as type 'unknown' in postgres, where the function lookup will fail due to incorrect type signature
+        action_string = cast(action, String)
+        username = cast(user.username, String)
+        proc = procs.exam_sale_action(cash_box_ids[cashbox], action_string, final_price, username, pages, _tax_group, config.FS_CONFIG['PRICE_PER_PAGE'] / 100)
+        sqla.session.execute(proc)
+
 
     def log_exam_sale(pages: int, price: int, user, cashbox: str):
         _log_exam_action(pages, price / 100, user, cashbox, 'EXAM_SALE')
+
 
     def log_erroneous_sale(price: int, user, cashbox: str):
         """price is assumed to be positive"""
         _log_exam_action(0, price / -100, user, cashbox, 'EXAM_SALE_CANCEL')
 
 
-    def _log_deposit_action(deposit, user, cashbox: str, amount: int, action: str):
-        cshbx_log_id = _cash_box_log_entry(user, cashbox, amount, action)
-        qry = text("""INSERT INTO garfield.exam_deposit_log
-                (student_name, cash_box_log_id)
-                VALUES (:student_name, :log_id);""")
-        sqla.session.execute(qry.bindparams(
-            student_name=deposit.name,
-            log_id=cshbx_log_id
-        ))
+    def _log_deposit_action(deposit, user, cashbox: str, final_amount: float, action: str):
+        # Without these casts, the strings will end up as type 'unknown' in postgres, where the function lookup will fail due to incorrect type signature
+        username = cast(user.username, String)
+        action_string = cast(action, String)
+        deposit_name = cast(deposit.name, String)
+        proc = procs.exam_deposit_action(cash_box_ids[cashbox], action_string, final_amount, username, deposit_name)
+        sqla.session.execute(proc)
+
 
     def log_deposit(deposit, user, cashbox: str):
         _log_deposit_action(deposit, user, cashbox,
