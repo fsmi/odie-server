de89f36421cd904a65c5ea91b5ac0d8c32ec5ef2
Jan Unterbrink <jan.unterbrink@subpath.de>
2018-05-12 11:31:25 +0200

    written functionality of user hash

--- db/userHash.py
+++ db/userHash.py
@@ -1,10 +1,26 @@
 #! /usr/bin/env python3
 
 import config
-from odie import sqla
+from odie import sqla, Column
+
+import sqlalchemy
+from sqlalchemy import func
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.orm import query
+from sqlalchemy.sql import select
+
+from db import garfield
 
 # this class you can use to generate an collision free unique hash of an specific transcation
 class userHash:
 
     # this function returns the last used id
     def returnLastUsedId(self):
+        multprimary = 104729
+        difprimary = 5
+        # set table and table args of the last transaction
+
+        id = sqla.text("""SELECT max(id) FROM orders""")
+        res = (id * multprimary) / difprimary
+        return res
+
