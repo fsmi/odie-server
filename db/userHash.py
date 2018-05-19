88827db6d6ae662cd55a100a8c2d8c1259546bc2
Jan Unterbrink <jan.unterbrink@subpath.de>
2018-05-19 20:34:55 +0200

    added setLength function

--- db/userHash.py
+++ db/userHash.py
@@ -19,6 +19,19 @@ from db import garfield
 # this class you can use to generate an collision free unique hash of an specific transcation
 class userHash:
 
+    # this function set the length of the string
+    # it retuns an id in the coressponding length
+    def setLength(integer):
+        defined_length = 8
+        ret = ""
+
+        for i in range(0, defined_length):
+            if integer < pow(10,i):
+                ret += '0'
+
+        ret += str(integer)
+        return ret
+
     # this function returns the last used id
     # @param table is the table which should be used
     def returnIdSales(self):
@@ -28,7 +41,7 @@ class userHash:
 
             min = 0
             max = 99999999
-            rand = 's' + str(random.randint(min, max))
+            rand = 's' + self.setLength(random.randint(min, max))
 
             db = Deposit.query.filter(Deposit.name == rand)
             if db is None:
