4dadf09797b4f0ab8f9bf04dc579c9b643b48748
Jan Unterbrink <jan.unterbrink@subpath.de>
2018-05-20 08:54:32 +0200

    added own Exception class

--- db/userHash.py
+++ db/userHash.py
@@ -17,6 +17,13 @@ from pytz import reference
 
 from db import garfield
 
+# this class is to handle Exceptions
+class ToManyAttempts(Exception):
+    def __init__(self, message, errors):
+        super.__init__(message)
+
+        self.errors = errors
+
 # this class you can use to generate an collision free unique hash of an specific transcation
 class userHash:
 
@@ -56,7 +63,7 @@ class userHash:
     def returnIdCard(self):
         for i in range(0,6):
             if i == 6:
-                raise Exception('to many attempts')
+                raise ToManyAttempts('to many attempts')
 
             min = 0
             max = 99999999
