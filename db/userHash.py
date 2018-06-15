017d05823c916e0b0f2c41f82eb6aee1d17cd513
Jan Unterbrink <jan.unterbrink@subpath.de>
2018-06-15 13:19:05 +0200

    fix cerate id document submission

--- db/userHash.py
+++ db/userHash.py
@@ -84,9 +84,8 @@ class userHash:
             max = 99999999
             rand = 'u' + self.setLength(random.randint(min, max))
 
-            break
-
-            db = Document.query.filter(submitted_by = rand).first()
+            db = Document.query.filter_by(submitted_by = rand).first()
+            print(db)
             if db is None:
                 break
 
